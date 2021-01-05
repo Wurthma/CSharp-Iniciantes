1d77d01153bc16fe3428d9b6bcd2e6066fd5d300
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-05 00:57:30 -0300

    conclusão calculadora

--- 02_Calculadora/Calculadora/Program.cs
+++ 02_Calculadora/Calculadora/Program.cs
@@ -6,7 +6,146 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();            
+        }
+
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            Console.WriteLine("5 - Resto da divisão");
+            Console.WriteLine("6 - Potenciação");
+            Console.WriteLine("0 - Sair");
+
+            string opcao = Console.ReadLine();
+
+            switch(opcao)
+            {
+                case "1":
+                    Somar();
+                    break;
+                case "2":
+                    Subtrair();                    
+                    break;
+                case "3":
+                    Multiplicar();                    
+                    break;
+                case "4":
+                    Dividir();                    
+                    break;
+                case "5":
+                    EncontrarRestoDivisao();                    
+                    break;
+                case "6":
+                    CalcularPotenciacao();                    
+                    break;
+                case "0":
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        public static void Somar()
+        {
+            double valor1, valor2;
+
+            Console.WriteLine("Entre com o primeiro valor:");
+            valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o segundo valor:");
+            valor2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{valor1} + {valor2} = {valor1 + valor2}");
+            Console.ReadLine();
+            Menu();
+        }
+
+        public static void Subtrair()
+        {
+            double valor1, valor2;
+
+            Console.WriteLine("Entre com o primeiro valor:");
+            valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o segundo valor:");
+            valor2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{valor1} - {valor2} = {valor1 - valor2}");
+            Console.ReadLine();
+            Menu();
+        }
+
+        public static void Multiplicar()
+        {
+            double valor1, valor2;
+
+            Console.WriteLine("Entre com o primeiro valor:");
+            valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o segundo valor:");
+            valor2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{valor1} * {valor2} = {valor1 * valor2}");
+            Console.ReadLine();
+            Menu();
+        }
+
+        public static void Dividir()
+        {
+            double dividendo, divisor;
+
+            Console.WriteLine("Informe o dividendo:");
+            dividendo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o divisor:");
+            divisor = double.Parse(Console.ReadLine());
+
+            if(divisor != 0)
+                Console.WriteLine($"{dividendo} / {divisor} = {dividendo / divisor}");
+            else
+                Console.WriteLine("Não é possível dividir por zero.");
+
+            Console.ReadLine();
+            Menu();
+        }
+
+        // Desafios
+        // 1- Crie uma nova opção na calculadora (opção número 5 do menu) que retorne o resto da divisão.
+        public static void EncontrarRestoDivisao()
+        {
+            double dividendo, divisor;
+
+            Console.WriteLine("Informe o dividendo:");
+            dividendo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o divisor:");
+            divisor = double.Parse(Console.ReadLine());
+
+            if(divisor != 0)
+                Console.WriteLine($"Resto entre {dividendo} e {divisor} = {dividendo % divisor}");
+            else
+                Console.WriteLine("Não é possível dividir por zero.");
+
+            Console.ReadLine();
+            Menu();
+        }
+
+        // Desafios
+        // 2- Crie uma nova opção na calculadora (opção número 6 do menu) que retorne o resultado da potenciação de uma determinada base pelo seu expoente. Exemplo **2³ = 8**.
+        public static void CalcularPotenciacao()
+        {
+            double basePotenciacao, expoente;
+
+            Console.WriteLine("Informe a base:");
+            basePotenciacao = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o expoente:");
+            expoente = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{basePotenciacao} elevado a {expoente} = {Math.Pow(basePotenciacao, expoente)}");
+
+            Console.ReadLine();
+            Menu();
         }
     }
 }
--- 02_Calculadora/README.md
+++ 02_Calculadora/README.md
@@ -1,3 +1,28 @@
 ### Calculadora simples
 
-Em construção...
+**Objetivo:** usando uma aplicação do tipo console do dotnet, criar uma calculadora simples que terá as operações de soma, subtração, multiplicação e divisão.
+
+**Requisitos:** 
+- Ao entrar na calculadora o usuário deverá ter um menu com as opções 1, 2, 3, 4 e 0. Sendo elas:
+
+	1 - Somar
+	2 - Subtrair
+	3 - Multiplicar
+	4 - Dividir
+	0 - Sair
+
+- Todas as operações devem solicitar que o usuário entre com dois valores e então estes valores devem ser utilizados para realizar a operação escolhida. Exemplo de fluxo da operação de Soma:
+	1. Aplicação solicita o primeiro valor;
+	2. Usuário digita valor e pressiona enter;
+	3. Aplicação solicita o segundo valor;
+	4. Usuário digita valor e pressiona enter;
+	5. Aplicação escreve em tela o resultado da soma;
+	6. Usuário pressiona Enter;
+	7. Aplicação retorna para o menu;
+
+- Para a operação de divisão, validar se o divisor é 0. Se verdadeiro deve ser exibida a mensagem "Não é possível dividir por zero."
+
+# Desafios
+
+1- Crie uma nova opção na calculadora (opção número 5 do menu) que retorne o resto da divisão.
+2- Crie uma nova opção na calculadora (opção número 6 do menu) que retorne o resultado da potenciação de uma determinada base pelo seu expoente. Exemplo **2³ = 8**.
