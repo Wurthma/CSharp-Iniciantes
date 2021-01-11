a1bf8b326e89ee88229da5c44ddbc1ae2d8abf7d
George Henrique Wurthmann <gemakai@gmail.com>
2021-01-10 23:53:18 -0300

    Iniciando exercício OO

---
+++ 08_OrientacaoObjetos/DroneExploracao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DroneExploracao
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Em construção...
+        }
+    }
+}
